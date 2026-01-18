755c35a884aaa52ca232cb29f506fd12aee34dde
ixwelx <ridiixwelx@gmail.com>
2026-01-18 23:22:06 +0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,5 @@ import App from './App.jsx'
 import { ConfigProvider } from "antd";
 
 createRoot(document.getElementById('root')).render(
-    // <ConfigProvider
-    //     theme={{
-    //         token: {
-    //             colorPrimary: "#7f3fbf",
-    //             colorBgBase: "#0f0b14",
-    //             colorTextBase: "#e8dff5",
-    //             borderRadius: 10,
-    //         },
-    //     }}
-    // >
         <App />
-    // </ConfigProvider>
 )
